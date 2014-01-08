15b44f6055f88643cb566cd469341c3c915009f1
Brent Ertz <brent.ertz@gmail.com>
2014-01-08 09:17:43 -0700

    Remove pages not in use

--- lib/mustachio/app.rb
+++ lib/mustachio/app.rb
@@ -6,11 +6,11 @@ module Mustachio
     DEMO_IMAGE = 'http://quickleft-production.s3.amazonaws.com/uploads/user/team_page_sprite/2/Ingrid_Large.png'
 
     set :static, true
-    
+
     configure :production do
       require 'newrelic_rpm' if ENV['NEW_RELIC_ID']
     end
-    
+
     before do
       app_host = ENV['MUSTACHIO_APP_DOMAIN']
       if app_host && request.host != app_host
@@ -18,12 +18,10 @@ module Mustachio
         redirect request.url.sub(request_host_with_port, app_host), 301
       end
     end
-    
-    
+
     get %r{^/(\d+|rand)?$} do |stache_num|
       src = params[:src]
       if src
-        # use the specified stache, otherwise fall back to random
         image = Magickly.process_src params[:src], :mustachify => (stache_num || 'rand')
         image.to_response(env)
       else
@@ -31,18 +29,6 @@ module Mustachio
         @site = Addressable::URI.parse(request.url).site
         haml :index
       end
-    end
-    
-    get '/gallery' do
-      haml :gallery
-    end
-    
-    get '/test' do
-      haml :test
-    end
-    
-    get '/face_api_dev_challenge' do
-      haml :face_api_dev_challenge
     end
 
     get '/ping' do
